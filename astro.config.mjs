56c7b4096ce11d0c1b529273c37ee92fd266cd35
Manu Sharma <manu@d365mastery.com>
2026-09-16 18:51:24 +0530

    Restore custom domain and base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://manusharmaindia.github.io',
-  base: '/D365Mastery',
+  site: 'https://d365mastery.com',
+  base: '/',
   trailingSlash: 'never',
   integrations: [
     tailwind({
